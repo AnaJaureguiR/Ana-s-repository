19f4d25f9d822ba26f282eecb0e2a0ba2ea296cc
Ana Jáuregui <146563607+AnaJaureguiR@users.noreply.github.com>
2024-05-10 09:11:19 +0200

    Create pruebas - copia.java

---
+++ pruebas - copia.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class pruebas {
+
+        public static void main(String[] args) {
+       Scanner in = new Scanner (System.in);
+        int a, b;
+
+        System.out.println("Introduce a:");
+        a = in.nextInt();
+
+        System.out.println("Introduce b:");
+        b = in.nextInt();
+
+        System.out.println(a + b);
+       
+    }
+}
